9c05d2f512287f3211aed5b0d6c7ab98fba8adeb
joshgressman <joshgressman@gmail.com>
2016-11-30 18:47:52 -0600

    connected DB

---
+++ admin/includes/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database Connection Constance
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','gallery_db');
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+//Check if connected
+if($connection) {
+  echo "true";
+}
+
+?>
